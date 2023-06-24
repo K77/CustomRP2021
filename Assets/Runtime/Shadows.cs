5d9204ee78fc7170a9806d148ca7c247cd3483b1
k <k@macbook.air>
2023-06-24 15:00:18 +0800

    03. 多光源的影子已经可以显示了, 下一步搞级联阴影

--- Assets/Runtime/Shadows.cs
+++ Assets/Runtime/Shadows.cs
@@ -44,17 +44,27 @@ public class Shadows {
         this.settings = settings;
     }
 
-    public void ReserveDirectionalShadows(Light light, int visibleLightIndex)
+    public Vector2 ReserveDirectionalShadows(Light light, int visibleLightIndex)
     {
         if (ShadowedDirectionalLightCount < maxShadowedDirectionalLightCount  &&
             light.shadows != LightShadows.None && light.shadowStrength > 0f &&
             cullingResults.GetShadowCasterBounds(visibleLightIndex, out Bounds b) )
         {
-            ShadowedDirectionalLights[ShadowedDirectionalLightCount++] =
+            // ShadowedDirectionalLights[ShadowedDirectionalLightCount++] =
+            //     new ShadowedDirectionalLight {
+            //         visibleLightIndex = visibleLightIndex
+            //     };
+            ShadowedDirectionalLights[ShadowedDirectionalLightCount] =
                 new ShadowedDirectionalLight {
                     visibleLightIndex = visibleLightIndex
                 };
+            return new Vector2(
+                //shadowStrength越小，影子越淡
+                light.shadowStrength, ShadowedDirectionalLightCount++
+            );
         }
+
+        return Vector2.zero;
     }
     
     public void Render () {
@@ -84,7 +94,7 @@ public class Shadows {
         for (int i = 0; i < ShadowedDirectionalLightCount; i++) {
             RenderDirectionalShadows(i, split,tileSize);
         }
-		
+        buffer.SetGlobalMatrixArray(dirShadowMatricesId, dirShadowMatrices);
         buffer.EndSample(bufferName);
         ExecuteBuffer();
     }
@@ -112,17 +122,51 @@ public class Shadows {
         );
         
         shadowSettings.splitData = splitData;
-        SetTileViewport(index, split, tileSize);
+        // SetTileViewport(index, split, tileSize);
+        dirShadowMatrices[index] = ConvertToAtlasMatrix(
+            projectionMatrix * viewMatrix,
+            SetTileViewport(index, split, tileSize), split
+        );
+        // dirShadowMatrices[index] = projectionMatrix * viewMatrix;
         buffer.SetViewProjectionMatrices(viewMatrix, projectionMatrix);
         
         ExecuteBuffer();
         context.DrawShadows(ref shadowSettings);
     }
-    void SetTileViewport (int index, int split, float tileSize) {
+    Vector2 SetTileViewport (int index, int split, float tileSize) {
         Vector2 offset = new Vector2(index % split, index / split);
         buffer.SetViewport(new Rect(
             offset.x * tileSize, offset.y * tileSize, tileSize, tileSize
         ));
+        return offset;
+    }
+    
+    Matrix4x4 ConvertToAtlasMatrix(Matrix4x4 m, Vector2 offset, int split)
+    {
+        //如果使用反向Z缓冲区，为Z取反
+        if (SystemInfo.usesReversedZBuffer)
+        {
+            m.m20 = -m.m20;
+            m.m21 = -m.m21;
+            m.m22 = -m.m22;
+            m.m23 = -m.m23;
+        }
+        //光源裁剪空间坐标范围为[-1,1]，而纹理坐标和深度都是[0,1]，因此，我们将裁剪空间坐标转化到[0,1]内
+        //然后将[0,1]下的x,y偏移到光源对应的Tile上
+        float scale = 1f / split;
+        m.m00 = (0.5f * (m.m00 + m.m30) + offset.x * m.m30) * scale;
+        m.m01 = (0.5f * (m.m01 + m.m31) + offset.x * m.m31) * scale;
+        m.m02 = (0.5f * (m.m02 + m.m32) + offset.x * m.m32) * scale;
+        m.m03 = (0.5f * (m.m03 + m.m33) + offset.x * m.m33) * scale;
+        m.m10 = (0.5f * (m.m10 + m.m30) + offset.y * m.m30) * scale;
+        m.m11 = (0.5f * (m.m11 + m.m31) + offset.y * m.m31) * scale;
+        m.m12 = (0.5f * (m.m12 + m.m32) + offset.y * m.m32) * scale;
+        m.m13 = (0.5f * (m.m13 + m.m33) + offset.y * m.m33) * scale;
+        m.m20 = 0.5f * (m.m20 + m.m30);
+        m.m21 = 0.5f * (m.m21 + m.m31);
+        m.m22 = 0.5f * (m.m22 + m.m32);
+        m.m23 = 0.5f * (m.m23 + m.m33);
+        return m;
     }
     void ExecuteBuffer () {
         context.ExecuteCommandBuffer(buffer);
