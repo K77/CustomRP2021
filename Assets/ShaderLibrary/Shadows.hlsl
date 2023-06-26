0b0a6d99c110f3819762fb8d22088666c646c98b
K_XPS15 <jizj@icloud.com>
2023-06-26 16:07:52 +0800

    03. 4.7 - Blending Cascades 当前场景分级阴影交界处就有问题

--- Assets/ShaderLibrary/Shadows.hlsl
+++ Assets/ShaderLibrary/Shadows.hlsl
@@ -53,6 +53,7 @@ float _FadedShadowStrength (float distance, float scale, float fade) {
 
 ShadowData GetShadowData (Surface surfaceWS) {
     ShadowData data;
+    data.cascadeBlend = 1.0;
     data.strength = _FadedShadowStrength(
         surfaceWS.depth, _ShadowDistanceFade.x, _ShadowDistanceFade.y
     );
@@ -61,11 +62,14 @@ ShadowData GetShadowData (Surface surfaceWS) {
         float4 sphere = _CascadeCullingSpheres[i];
         float distanceSqr = DistanceSquared(surfaceWS.position, sphere.xyz);
         if (distanceSqr < sphere.w) {
+            float fade = _FadedShadowStrength(
+                distanceSqr, _CascadeData[i].x, _ShadowDistanceFade.z
+            );
             if (i == _CascadeCount - 1) {
-                // data.strength = 0;
-                data.strength *= _FadedShadowStrength(
-                    distanceSqr, _CascadeData[i].x, _ShadowDistanceFade.z
-                );
+                data.strength *= fade;
+            }
+            else {
+                data.cascadeBlend = fade;
             }
             break;
         }
